69aff5947d8e253a8021c027e6cba575585c4269
glenn <glenn@glenns-MacBook-Pro.local>
2016-09-23 09:24:53 -0500

    Added create_vote_file() function

--- code/SingleVoteScraper.py
+++ code/SingleVoteScraper.py
@@ -1,7 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
-#html = file('/Users/glenn/Zipfian/project/congress/data/view-source_clerk.house.gov_evs_2016_roll520.xml', 'r')
+html = file('/Users/glenn/Zipfian/project/congress/data/view-source_clerk.house.gov_evs_2016_roll520.xml', 'r')
+
+def get_single_vote(soup, o_file):
+    # Get name-id, and sort-field from legislator tag
+    legislator_attrs = ['name-id', 'sort_field']
+    legislator_data = scrape_attrs(soup, "legislator", legislator_attrs)
+    # Get votes from <vote> tag
+    votes = scrape_tag_text(soup, 'vote')
+    # Make sure that legislator names match the specified file
+
+    # Append them to the specified file
+    pass
+
+def scrape_tag_text(soup, tag):
+    output = []
+    for node in soup.findAll(tag):
+        output.append(node.find(text=True) )
+    return output
 
 def get_legislator_table(soup, o_file):
     legislator_attrs = ['name-id', 'party', 'role', 'sort-field', 'state', 'unaccented-name']
@@ -28,6 +46,27 @@ def file_output(data, filename):
         for d in data:
             f.write(d + "\n")
 
+def create_vote_file(ip_file, op_file):
+    # Create a new text file for vote outputs
+    # first, check the file to make sure that doesn't exist yet
+    if os.path.isfile(op_file): # fixNote - Add proper path check
+        print "File already exists, please choose another."
+        return None
+    # Split the ip_file on commas, and retireve the first column
+    legislator_ids = []
+    with open(ip_file, 'r') as i_file:
+        for line in i_file:
+            legislator_ids.append(line.split(',')[0])
+    header = 'issue\t' + "\t".join(legislator_ids)
+    with open(op_file, 'w') as o_file:
+        o_file.write(header)
+
+    # if it's new, add issue, and all of the legislator name_ids to it
+    #with open(op_file) as f:
+
 if __name__ == '__main__':
-    soup = pull_page('http://clerk.house.gov/evs/2016/roll520.xml')
-    get_legislator_table(soup, '../data/legislators_house_114th_2nd.txt')
+    #soup = pull_page('http://clerk.house.gov/evs/2016/roll520.xml')
+    #get_legislator_table(soup, '../data/legislators_house_114th_2nd.txt')
+    ip_file = '/Users/glenn/Zipfian/project/congress/data/legislators_house_114th_2nd.txt'
+    op_file = '/Users/glenn/Zipfian/project/congress/data/votes_house_114th_2nd.txt'
+    create_vote_file(ip_file, op_file)
